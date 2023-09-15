09cbe181e0bdff8c45b49fef45a5ae2dd410c4f1
Ladson Yuri <ladsonyurip@gmail.com>
2023-09-15 08:43:23 -0300

    first commit

---
+++ v1.py
@@ -0,0 +1,11 @@
+fila = []
+
+fila.append(1)
+fila.append(2)
+fila.append(3)
+
+
+Obejeto = fila.pop(0)
+print("Removido:", Obejeto)
+
+print(fila)
---
+++ v2.py
@@ -0,0 +1,11 @@
+pilha = []
+
+pilha.append(1)
+pilha.append(2)
+pilha.append(3)
+
+
+Objeto = pilha.pop()
+print("Removido:", Objeto)
+
+print(pilha)
